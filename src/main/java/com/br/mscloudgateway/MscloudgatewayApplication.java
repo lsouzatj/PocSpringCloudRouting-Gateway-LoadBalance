35533e284ab78792c27297d886dbf9fa7bbb4119
lsouzatj <lsouza.tj@gmail.com>
2022-09-17 14:22:55 -0300

    Implementacao Gateway e LodBalance, e registrando o servico no Eureka

--- src/main/java/com/br/mscloudgateway/MscloudgatewayApplication.java
+++ src/main/java/com/br/mscloudgateway/MscloudgatewayApplication.java
@@ -2,12 +2,26 @@ package com.br.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class MscloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder
+				.routes()
+				.route(r -> r.path("/cliente/**").uri("lb://msclientes"))
+				.build();
+	}
 }
