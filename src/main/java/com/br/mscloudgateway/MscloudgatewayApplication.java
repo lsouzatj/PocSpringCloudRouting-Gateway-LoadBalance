27a89c019112cdd9d6c757517b9889f4a4d07c8c
lsouzatj <lsouza.tj@gmail.com>
2022-09-17 14:10:39 -0300

    Criacao do projeto

---
+++ src/main/java/com/br/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.br.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+}
